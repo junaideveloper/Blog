c9eaf4c26416df927bf5a854311d7fbde39291c6
etisalat <etisalat@L00087634.etisalat.corp.ae>
2020-12-08 22:37:57 +0400

    Blog post with static file handling

--- myblog/urls.py
+++ myblog/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path,include
 from django.contrib.auth import views as auth_views
+from django.conf.urls.static import static
 
+from django.conf import settings
 
 from personal.views import home_screen_view
 
@@ -54,3 +56,6 @@ urlpatterns = [
     
 
 ]
+if settings.DEBUG:
+    urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
